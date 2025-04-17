7e0f4de555a0ad4c1f5e2a7acb4a54c2e2039024
Sebastian <sebastiankasimirsassi@gmail.com>
2025-04-17 18:56:00 +0300

    added maybe_unused

--- include/zernike_conventions.hpp
+++ include/zernike_conventions.hpp
@@ -70,7 +70,7 @@ template <ZernikeNorm from, ZernikeNorm to>
 
 template <ZernikeNorm from, ZernikeNorm to>
     requires (from == to)
-[[nodiscard]] inline double conversion_factor(std::size_t n) noexcept
+[[nodiscard]] inline double conversion_factor([[maybe_unused]] std::size_t n) noexcept
 {
     return 1.0;
 }
