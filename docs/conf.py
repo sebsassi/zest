24d5f9d3638cf0934c00f7718780fd08594dd6e9
Sebastian <sebastiankasimirsassi@gmail.com>
2025-04-27 01:09:46 +0300

    tuned pdf doc appearance

--- docs/conf.py
+++ docs/conf.py
@@ -17,8 +17,8 @@ print(sys.executable)
 
 extensions = ['breathe']
 
-breathe_projects = {'zest': '_build/xml'}
-breathe_default_project = "zest"
+breathe_projects = {project: '_build/xml'}
+breathe_default_project = project
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -32,11 +32,43 @@ html_static_path = ['_static']
 
 # -- Options for LaTeX output ------------------------------------------------
 
+project_name = 'zest'
+
 latex_engine = 'lualatex'
-latex_documents = [(master_doc, 'zest_doc.tex', 'ZebraDM', author, 'manual', False)]
+latex_documents = [(master_doc, f'{project}_doc.tex', project_name, author, 'manual', False)]
 latex_toplevel_sectioning = 'section'
 latex_elements = {
     'papersize': 'a4paper',
-    'preamble': '',
-    'extrapackages': r'\usepackage{microtype}'
+    'fontpkg': r'''
+\usepackage{unicode-math}
+\setmainfont{STIXTwoText}[BoldFont=Stix Two Text Semibold]
+\setsansfont{OpenSans}
+\setmonofont{NotoSansMono}[Scale=0.91, BoldFont=Noto Sans Mono Bold]
+\setmathfont{StixTwoMath}[StylisticSet=1]
+    ''',
+    'preamble': r'''
+\newcommand{\subtitle}{Zernike and Spherical harmonic Transforms}
+\newcommand{\DUrolek}[1]{\textbf{\texttt{#1}}}
+\newcommand{\DUrolekt}[1]{\texttt{#1}}
+\newcommand{\DUrolen}[1]{\texttt{#1}}
+\newcommand{\DUrolep}[1]{\texttt{#1}}
+''',
+    'extrapackages': r'''
+\usepackage{microtype}
+''',
+    'releasename': 'Version',
+    'maketitle': r'''
+\makeatletter
+\begin{titlepage}
+\begin{center}
+\vspace*{18pc}
+{\Huge\textsf{\textbf{\texttt{\@title}}}}\\[2pc]
+{\Large\textsf{\textbf{\subtitle}}}\\[2pc]
+{\large\textsf{\@author}}
+\vfill
+{\normalsize\textsf{\texttt{\py@release\releaseinfo}\hfill\@date}}
+\end{center}
+\end{titlepage}
+\makeatother
+'''
 }
