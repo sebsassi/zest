0d4a6d678f0c9a5a7c59bd525481ff0a0b57f84a
seb <posadowskybreen@proton.me>
2026-01-26 14:07:30 +0200

    ... and second constructor argument

--- include/sh_generator.hpp
+++ include/sh_generator.hpp
@@ -228,7 +228,7 @@ public:
     [[nodiscard]] SHExpansion<double, indexing_mode, sh_norm, sh_phase, std::dynamic_extent>
     generate(std::span<const double> lon, std::span<const double> colat, std::size_t order)
     {
-        SHExpansion<double, indexing_mode, sh_norm, sh_phase, std::dynamic_extent> expansion{order};
+        SHExpansion<double, indexing_mode, sh_norm, sh_phase, std::dynamic_extent> expansion{order, lon.size()};
         generate(lon, colat, expansion);
         return expansion;
     }
